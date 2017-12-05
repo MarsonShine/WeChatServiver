3de95bbeb466fe98ee1b02587492aaeddb487f75
MarsonShine <ms27946@163.com>
2017-12-06 00:05:39 +0800

    add:增加测试网站

---
+++ WeChatService/WxWebsite/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeChatService.Service;
+
+namespace WxWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly WxHandle _wx;
+        private readonly WeChatConfigurationProvider _provider;
+        public HomeController()
+        {
+            _wx = new WxHandle();
+            _provider = new WeChatConfigurationProvider(System.Web.Hosting.HostingEnvironment.MapPath("~/wx.config.json"));
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Authentication()
+        {
+            //获取token
+            var accessToken = _wx.GetAccessToken().GetAwaiter().GetResult();
+            _wx.GetWxWebAuthenticationCode(accessToken, "http://localhost:8081/Home/AuthenticationCode").GetAwaiter().GetResult();
+            return View();
+        }
+
+        public ActionResult AuthenticationCode()
+        {
+            return new EmptyResult();
+        }
+    }
+}
